8513cf98ff236114178a3af5647a80d9933e02d6
Renato <reytuty@gmail.com>
2023-08-16 09:56:29 -0300

    fix: changes to import module in ts

--- src/example/usage.test.ts
+++ src/example/usage.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
 
-import { Singleton } from "..";
+import { Singleton } from "typescript-singleton";
 
 class MyClassThatIWannaToBeUnique {
   public name: string;
@@ -19,14 +19,14 @@ describe("testing instance two classes", () => {
       Singleton.getInstance<MyClassThatIWannaToBeUnique>(
         privateKey,
         MyClassThatIWannaToBeUnique,
-        "test",
+        "test 1",
         total
       );
     const myClassInstance2: MyClassThatIWannaToBeUnique =
       Singleton.getInstance<MyClassThatIWannaToBeUnique>(
         privateKey,
         MyClassThatIWannaToBeUnique,
-        "test",
+        "test 2",
         total
       );
     //to prove that be the same instance, I will change a name of instance 1
@@ -34,5 +34,27 @@ describe("testing instance two classes", () => {
     expect(myClassInstance1).toBe(myClassInstance2);
     expect(myClassInstance2 === myClassInstance1).toBe(true);
     expect(myClassInstance1.name).toBe(myClassInstance2.name);
+  });
+  it("must be the distinct instances", () => {
+    const privateKey: string = "myprivatekeyhere";
+    const total = 10;
+    const myClassInstance1: MyClassThatIWannaToBeUnique =
+      Singleton.getInstance<MyClassThatIWannaToBeUnique>(
+        privateKey,
+        MyClassThatIWannaToBeUnique,
+        "test 1",
+        total
+      );
+    const myClassInstance3: MyClassThatIWannaToBeUnique =
+      Singleton.getInstance<MyClassThatIWannaToBeUnique>(
+        privateKey + "_changing_key",
+        MyClassThatIWannaToBeUnique,
+        "test 3",
+        total
+      );
+    //to prove that be the same instance, I will change a name of instance 1
+    myClassInstance1.name = "There is no test anymore";
+    expect(myClassInstance1.name).not.toBe(myClassInstance3.name);
+    expect(myClassInstance1 === myClassInstance3).not.toBe(true);
   });
 });
